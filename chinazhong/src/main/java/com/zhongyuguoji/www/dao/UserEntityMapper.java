303a0d1f60600cd70e0a5b522da2237c1e635ad4
Moese <yu.tian@futuremove.cn>
2020-01-07 15:44:52 +0800

    修改代码

--- chinazhong/src/main/java/com/zhongyuguoji/www/dao/UserEntityMapper.java
+++ chinazhong/src/main/java/com/zhongyuguoji/www/dao/UserEntityMapper.java
@@ -4,9 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.zhongyuguoji.www.entity.Integraml;
+import com.zhongyuguoji.www.entity.UserEntity;
 @Repository
 @Transactional
-public interface UserEntityMapper extends JpaRepository<Integraml, Integer> {
+public interface UserEntityMapper extends JpaRepository<UserEntity, Integer> {
 
 }
